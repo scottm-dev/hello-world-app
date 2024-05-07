a429560b56ab3050ffcdfbc49b4eb24834dcf5d1
scottm-dev <105765424+scottm-dev@users.noreply.github.com>
2024-05-06 19:58:12 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
+      TEST Hello World.{' '}
       <Link href="/about">
         <a>About</a>
       </Link>
